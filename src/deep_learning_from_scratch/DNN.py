1038097b650bcfa1dc42af7a35b31a0826d7da98
dmambapoor <dmambapoor@gmail.com>
2022-12-30 22:54:03 -0600

    Created a sample test-case to verify pytest.
    Restructured, renamed, and re-installed package. Added a sample test case and sample function to deep-learning class to test if pytest works properly.

---
+++ src/deep_learning_from_scratch/DNN.py
@@ -0,0 +1,8 @@
+
+class DNN:
+    
+    def __init__(self):
+        pass
+
+    def square(self, x):
+        return x * x
---
+++ tests/deep_learning_test.py
@@ -0,0 +1,6 @@
+from deep_learning_from_scratch.DNN import DNN
+class Tester:
+
+    def test_square(self):
+        model = DNN()
+        assert model.square(4) is 16
