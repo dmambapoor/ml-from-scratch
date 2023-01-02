02b2769b92677a4c84e642c6890edf162278311e
dmambapoor <dmambapoor@gmail.com>
2023-01-01 23:15:57 -0600

    Added functionality to build MLP layers.
    Note: currently number of layers is hard-coded.

--- src/deep_learning_from_scratch/MLP.py
+++ src/deep_learning_from_scratch/MLP.py
@@ -1,12 +1,41 @@
 import numpy as np
 
+
 class MLP:
 
     def __init__(self):
         pass
-    
-    def fit(self, x, y):
+
+    def fit(self, x: np.ndarray, y: np.ndarray):
+        # Construct multi-layer perceptron
+
+        # Add two hidden layers and one final layer consisting of only one node.
+
+        # Determine the sizes of the layers and biases -> add them to the array of layers
+        self.n_inputs = x.shape[1]
+        self.weights = []
+        self.biases = []
+
+        w1 = np.zeros(shape=(self.n_inputs, (self.n_inputs//2 + 1 if self.n_inputs//2 < 50 else 50)))
+        self.weights.append(w1)
+        b1 = np.zeros(shape=(1, w1.shape[1]))
+        self.biases.append(b1)
+
+        w2 = np.zeros(shape=(w1.shape[1], w1.shape[1]//2 + 1))
+        self.weights.append(w2)
+        b2 = np.zeros(shape=(1, w2.shape[1]))
+        self.biases.append(b2)
+
+        w3 = np.zeros(shape=(w2.shape[1], 1))
+        self.weights.append(w3)
+        b3 = np.zeros(shape=(w3.shape[1], 1))
+        self.biases.append(b3)
+
+    def feedforward(self, x):
         pass
+
+    def getAllLayers(self):
+        return self.weights, self.biases
 
     def square(self, x):
         return x * x
--- tests/deep_learning_test.py
+++ tests/deep_learning_test.py
@@ -1,7 +1,22 @@
+import numpy as np
 from deep_learning_from_scratch.MLP import MLP
 
 
 class Tester:
+
+    def test_init(self):
+        model = MLP()
+        x = np.zeros(shape=(10, 150))
+        y = np.zeros(shape=(10, 1))
+        model.fit(x, y)
+        weights, biases = model.getAllLayers()
+        ans = [(150, 50),
+               (50, 26),
+               (26, 1)]
+        print(len(weights))
+        print(len(biases))
+        for i in range(len(weights)):
+            assert weights[i].shape == ans[i]
 
     def test_square(self):
         model = MLP()
